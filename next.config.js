e2e02b4994504955dfdcc64bdddf72fc1ccd10de
Matheus <matheusdaws66@gmail.com>
2024-11-17 22:13:03 -0300

    Alteracoes para deploy

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ["res.cloudinary.com"]
-    }
+    cache: {
+        cacheControl: 'no-store',
+    },
 }
 
 module.exports = nextConfig
